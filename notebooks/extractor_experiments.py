7d555577c578fc42c737ac83d48b12846ee35437
Michael Mann <mmann1123@gmail.com>
2020-06-08 10:03:54 -0400

    edits from desktop

--- notebooks/extractor_experiments.py
+++ notebooks/extractor_experiments.py
@@ -17,6 +17,8 @@ from glob import glob
 from IPython import get_ipython
 ipython = get_ipython()
 
+from numpy import where
+
 #%% rename tifs
 #import os
 #import re
@@ -32,7 +34,7 @@ ipython = get_ipython()
  
 search = r'F:\5year\aet_xr\aet_2012*.tif'
  
-with gw.open(search, band_names=['aet'],      ) as ds:
+with gw.open(search, band_names=['aet'],          ) as ds:
 
     ds = ds.chunk((len(ds.time), 1, 250, 250))
 
@@ -84,7 +86,7 @@ for name, func, args in [
 #                   ('linear_time_trend',linear_time_trend, {'param':"rvalue"})
 #                   ('quantile',quantile, {'q':"0.5"}),
 #                   ('maximum',maximum, {}),
-#                   ('minimum',minimum, {})
+                   ('minimum',minimum, {})
                    ]:
 
 
@@ -112,36 +114,100 @@ client
 
 print('go to http://localhost:8787/status for dask dashboard') 
 #%%   THIS APPROACH SEEM MUCH SLOWER 
+ipython.magic("%%time")
 
 
   
-mydict = {'quantile': {'q':"0.5"},'maximum':{}}
+mydict = { 'maximum':{} ,'quantile': {'q':"0.5"}}
  
 
-def get_xr_attr(function_name):
+def _get_xr_attr(function_name):
     return getattr(xr_fresh.feature_generators.feature_calculators,
                    function_name)
 
 
-def apply_fun_name(function_name, xr_data, band, **args):
+def _apply_fun_name(function_name, xr_data, band, args, **args):
 
-      out = get_xr_attr(function_name)(xr_data.sel(band=band).persist(),**args).compute()
-      out.coords['variable'] = band + "__" + function_name  
+      out = _get_xr_attr(function_name)(xr_data.sel(band=band).persist(),**args).compute()
+      out.coords['variable'] = band + "__" + function_name + 
       return out
 
-# works
-#{funct: get_xr_attr(funct)(ds.sel(band='aet').persist(),**args) for funct, args in mydict.items()}
-res = [apply_fun_name(function_name = funct,
-                      xr_data=ds,
-                      band='aet', 
-                      **args)  for funct, args in mydict.items()]
+def extract_features(xr_data, feature_dict, band, na_rm = True, dim='variable',**args):
+
+    """
+    Extract features from
+
+    * a :class:`xarray.DataArray` containing a time series of rasters
+
+    A :class:`xarray.DataArray` with the calculated features will be returned a 'variable'.
+
+    Examples
+    ========
+
+    >>>  f_dict = { 'maximum':{} },'quantile': {'q':"0.5"}}
+    >>>  features = extract_features(xr_data=ds,
+    >>>                     feature_dict=mydict,
+    >>>                     band='aet', 
+    >>>                     na_rm = True)
+
+    :param xr_data: The xarray.DataArray with a time series of rasters to compute the features for.
+    :type xr_data: xarray.DataArray
+
+    :param feature_dict: mapping from feature calculator names to parameters. Only those names
+           which are keys in this dict will be calculated. See example above. 
+    :type feature_dict: dict
+
+    :param band: The name of the variable to create feature for.
+    :type band: str
+
+    :param na_rm: If True (default), all missing values are masked using .attrs['nodatavals']
+    :type na_rm: bool
+
+    :param dim: The name of the dimension used to collect outputed features
+    :type dim: str
+    
+    :return: The DataArray containing extracted features in `dim`.
+    :rtype: xarray.DataArray
+    
+    """
+    
+    
+    if na_rm:
+        nodataval = xr_data.attrs['nodatavals'][where(ds.band.values==band)[0][0]]
+
+    features = [_apply_fun_name(function_name = funct,
+                      xr_data=xr_data.where(xr_data.sel(band=band) != nodataval),
+                      band= band, 
+                      args,
+                      **args)  for funct, args in feature_dict.items()]
+
+    return xr.concat(features, dim)
 
 
-F_C = xr.concat(res, dim='variable')
-out = F_C.sel(variable="aet__" + 'quantile')
+features = extract_features(xr_data=ds,
+                            feature_dict=mydict,
+                            band='aet', 
+                            na_rm = True)
+
+out = features.sel(variable="aet__" + 'quantile')
 out.plot.imshow()
 
-ipython.magic("time")
+
+
+#%%
+matrix = [[j for j in range(5)] for i in range(5)] 
+
+def _feature_arg_string(feature_dict):
+    for funct, args in feature_dict.items():
+        print((args))
+        #item+'_'+value for item, value in args.items()
+
+
+_feature_arg_string(mydict)
+#%%
+
+[item+'_'+value for item, value in args.items() for funct, args.items() in feature_dict.items()]
+            
 
 #%%
 
--- xr_fresh/feature_generators/extractors.py
+++ xr_fresh/feature_generators/extractors.py
@@ -1,77 +1,75 @@
 
 import logging
-import warnings
 
 import xarray as xr
 
 from xr_fresh.feature_generators import feature_calculators
 
+_logger = logging.getLogger(__name__)
 
-rechunked = ds.chunk((23, 1, 250, 250))
-
-print(rechunked.data)
-
-
-#%%
-%%time
-
-from dask.distributed import Client
-client = Client()
-client
-
-print('go to http://localhost:8787/status for dask dashboard') 
+from numpy import where
 
 
-res = []
-for name, func, args in [
-#                   ('abs_energy', abs_energy,{}),
-#                   ('mean_abs_change',mean_abs_change,{}),
-#                   ('variance_larger_than_standard_deviation',variance_larger_than_standard_deviation,{}),
-#                   ('ratio_beyond_r_sigma',ratio_beyond_r_sigma,{}),
-#                   ('large_standard_deviation',large_standard_deviation,{}),
-#                   ('symmetry_looking',symmetry_looking,{}),
-#                   ('sum_values',sum_values,{}),
-#                   ('autocorr',autocorr,{}),
-#                   ('cid_ce',cid_ce,{}),
-#                   ('mean_change',mean_change,{}),
-#                   ('mean_second_derivative_central',mean_second_derivative_central,{}),
-#                   ('median',median,{}),
-#                   ('mean',mean,{}),
-#                   ('length',length,{}),    
-#                   ('standard_deviation',standard_deviation,{}),
-#                   ('variance',variance,{}),
-#                   ('skewness',skewness,{}),
-#                   ('kurtosis',kurtosis,{}),
-#                   ('absolute_sum_of_changes', absolute_sum_of_changes,{}),
-#                   ('longest_strike_below_mean',longest_strike_below_mean,{}),
-#                   ('longest_strike_above_mean',longest_strike_above_mean,{}),
-#                   ('count_above_mean',count_above_mean,{}),
-                   ('first_doy_of_maximum',first_doy_of_maximum,{}),
-                   ('last_doy_of_maximum',last_doy_of_maximum,{}),
-                   ('last_doy_of_maximum',last_doy_of_maximum,{}),
-                   ('last_doy_of_minimum',last_doy_of_minimum,{}),
-                   ('first_doy_of_minimum',first_doy_of_minimum,{}),
-                   ('autocorrelation',autocorrelation,{'return_p':False,'lag':2}) , 
-                   ('ratio_value_number_to_time_series_length',ratio_value_number_to_time_series_length,{})  ,
-                   ('kendall_time_correlation',kendall_time_correlation,{}) ,  # very slow take out vectorize?
-                   ('linear_time_trend',linear_time_trend, {'param':"rvalue"}),
-                   ('quantile',quantile, {'q':"0.5"}),
-                   ('maximum',maximum, {}),
-                   ('minimum',minimum, {})
 
-                   ]:
+def _get_xr_attr(function_name):
+    return getattr(feature_calculators,
+                   function_name)
 
 
-    with ProgressBar():
-        y = func(rechunked.sel(band='NDVI').persist(),**args)  #previously used .load() this is faster
-        y.compute() 
-    y.coords['variable'] = "NDVI__" + name
-    res.append(y)
+def _apply_fun_name(function_name, xr_data, band, **args):
+
+      out = _get_xr_attr(function_name)(xr_data.sel(band=band).persist(),**args).compute()
+      out.coords['variable'] = band + "__" + function_name  
+      return out
+
+def extract_features(xr_data, feature_dict, band, na_rm = True, dim='variable',**args):
+
+    """
+    Extract features from
+
+    * a :class:`xarray.DataArray` containing a time series of rasters
+
+    A :class:`xarray.DataArray` with the calculated features will be returned a 'variable'.
+
+    Examples
+    ========
+
+    >>>  f_dict = { 'maximum':{} },'quantile': {'q':"0.5"}}
+    >>>  features = extract_features(xr_data=ds,
+    >>>                     feature_dict=mydict,
+    >>>                     band='aet', 
+    >>>                     na_rm = True)
+
+    :param xr_data: The xarray.DataArray with a time series of rasters to compute the features for.
+    :type xr_data: xarray.DataArray
+
+    :param feature_dict: mapping from feature calculator names to parameters. Only those names
+           which are keys in this dict will be calculated. See example above. 
+    :type feature_dict: dict
+
+    :param band: The name of the variable to create feature for.
+    :type band: str
+
+    :param na_rm: If True (default), all missing values are masked using .attrs['nodatavals']
+    :type na_rm: bool
+
+    :param dim: The name of the dimension used to collect outputed features
+    :type dim: str
     
- 
-F_C = xr.concat(res, dim='variable',)
-out = F_C.sel(variable="NDVI__" + name)
-out.plot.imshow()
+    :return: The DataArray containing extracted features in `dim`.
+    :rtype: xarray.DataArray
+    
+    """
+    
+    
+    if na_rm:
+        nodataval = xr_data.attrs['nodatavals'][where(xr_data.band.values==band)[0][0]]
 
-client.close()
+    # iterate across feature_dict elements and capture created features
+    features = [_apply_fun_name(function_name = funct,
+                      xr_data=xr_data.where(xr_data.sel(band=band) != nodataval),
+                      band= band, 
+                      **args)  
+                            for funct, args in feature_dict.items()]
 
+    return xr.concat(features, dim)
