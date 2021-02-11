ce7c5682f26de80dec5532dcbc9c4773ea04e6a8
mmann1123 <mmann1123@gmail.com>
2021-02-11 09:00:31 -0500

    cleanup

--- xr_fresh/extractors.py
+++ xr_fresh/extractors.py
@@ -1,11 +1,12 @@
-
 import logging
 import warnings
 import xarray as xr
+
 # from numpy import where
 from xr_fresh import feature_calculators
 from xr_fresh.utils import xarray_to_rasterio
 from itertools import chain
+
 # from os.path import expanduser
 # from os.path import join as path_join
 # from dask.distributed import progress
@@ -13,83 +14,116 @@ from itertools import chain
 
 _logger = logging.getLogger(__name__)
 
+
 def _exists(var):
-     return var in globals()
+    return var in globals()
 
-    # use of globals is problematic, applies to all 
 
-def _append_dict(join_dict, string='_'):
-    '''
+# use of globals is problematic, applies to all
+
+
+def _append_dict(join_dict, string="_"):
+    """
     creates strings from dictionary key and value pairs if dictionary exists 
-    '''
-    
-    assert isinstance(join_dict, str)
-    
-    return '_' * _exists(join_dict) + \
-                            string.join(['_'.join(map(str, chain.from_iterable(globals()[join_dict].items()))) 
-                            if _exists(join_dict) else '' ])
-    
+    """
 
-def _stringr(notstring):    
-    '_'.join(str(notstring))
+    assert isinstance(join_dict, str)
+
+    return "_" * _exists(join_dict) + string.join(
+        [
+            "_".join(map(str, chain.from_iterable(globals()[join_dict].items())))
+            if _exists(join_dict)
+            else ""
+        ]
+    )
+
+
+def _stringr(notstring):
+    "_".join(str(notstring))
 
 
 def _get_xr_attr(function_name):
-    return getattr(feature_calculators,  function_name)
+    return getattr(feature_calculators, function_name)
 
 
 def _month_subset(xr_data, args):
-        # subset by month 
-        xr_data = xr_data.where( (xr_data['time.month'] >= args['start_month']) & 
-                                 (xr_data['time.month'] <= args['end_month']) , 
-                                 drop=True)
-        
-        months = {k: args[k] for k in args.keys() & {'start_month', 'end_month'}} 
-        
-        for x in ['end_month', 'start_month']: args.pop(x, None)  # remove unneeded args
+    # subset by month
+    xr_data = xr_data.where(
+        (xr_data["time.month"] >= args["start_month"])
+        & (xr_data["time.month"] <= args["end_month"]),
+        drop=True,
+    )
 
-        return xr_data, args, months    
+    months = {k: args[k] for k in args.keys() & {"start_month", "end_month"}}
+
+    for x in ["end_month", "start_month"]:
+        args.pop(x, None)  # remove unneeded args
+
+    return xr_data, args, months
 
 
 def _apply_fun_name(function_name, xr_data, band, args):
     # apply function for large objects lazy
-    print('Extracting:  '+ function_name)
-    
+    print("Extracting:  " + function_name)
+
     global months  # required to check if exists w _exists
 
-    if 'start_month' and 'end_month' in args:
-        print('subsetting to month bounds')
+    if "start_month" and "end_month" in args:
+        print("subsetting to month bounds")
         xr_data, args, months = _month_subset(xr_data, args)
-    
- 
-    out = _get_xr_attr(function_name)(xr_data.sel(band=band),**args).compute()        
-    
 
-    if function_name == 'linear_time_trend' and args == {'param': 'all'}:
-        #handle exception for regression 
-        out.coords['variable'] = [band + "__" + function_name+'__' + x + _append_dict(join_dict='months') for x in ['intercept'  , 'slope','pvalue','rvalue']]
-        
+    out = _get_xr_attr(function_name)(xr_data.sel(band=band), **args).compute()
+
+    if (
+        function_name == "linear_time_trend"
+        or function_name == "linear_time_trend2"
+        and args == {"param": "all"}
+    ):
+        # handle exception for regression
+        out.coords["variable"] = [
+            band + "__" + function_name + "__" + x + _append_dict(join_dict="months")
+            for x in ["intercept", "slope", "pvalue", "rvalue"]
+        ]
+
     else:
-        
-        # out.coords['variable'] = band + "__" + function_name+'_'   + _append_dict(join_dict='args') + _append_dict(join_dict='months')             
-        out.coords['variable'] = band + "__" + function_name+'_'   + '_'.join(map(str, chain.from_iterable(args.items()))) + _append_dict(join_dict='months')             
+
+        # out.coords['variable'] = band + "__" + function_name+'_'   + _append_dict(join_dict='args') + _append_dict(join_dict='months')
+        out.coords["variable"] = (
+            band
+            + "__"
+            + function_name
+            + "_"
+            + "_".join(map(str, chain.from_iterable(args.items())))
+            + _append_dict(join_dict="months")
+        )
 
     return out
 
 
-
 def check_dictionary(arguments):
     for func, args in arguments.items():
-        if type(args) == list and len(args)==0:
-            warnings.warn(" Problem with feature_dict, should take the following form: feature_dict = { 'maximum':[{}] ,'quantile': [{'q':'0.5'},{'q':'0.95'}]} Not all functions will be calculated")
-            print(''' Problem with feature_dict, should take the following form: 
+        if type(args) == list and len(args) == 0:
+            warnings.warn(
+                " Problem with feature_dict, should take the following form: feature_dict = { 'maximum':[{}] ,'quantile': [{'q':'0.5'},{'q':'0.95'}]} Not all functions will be calculated"
+            )
+            print(
+                """ Problem with feature_dict, should take the following form: 
                     feature_dict = { 'maximum':[{}] ,'quantile': [{'q':'0.5'},{'q':'0.95'}]} 
-                    ***Not all functions will be calculated***''')       
+                    ***Not all functions will be calculated***"""
+            )
 
 
-def extract_features(xr_data, feature_dict, band, na_rm = False, 
-                    filepath=None, postfix=None,
-                    dim='variable', persist=False,  *args):
+def extract_features(
+    xr_data,
+    feature_dict,
+    band,
+    na_rm=False,
+    filepath=None,
+    postfix="",
+    dim="variable",
+    persist=False,
+    *args
+):
     """
     Extract features from
 
@@ -137,59 +171,59 @@ def extract_features(xr_data, feature_dict, band, na_rm = False,
     :return: The DataArray containing extracted features in `dim`.
     :rtype: xarray.DataArray
     
-    """    
+    """
     check_dictionary(feature_dict)
-    
+
     # improvement: check cluster status, have attribute "persist" for setting
-    # persistence of small in memory objects. 
+    # persistence of small in memory objects.
     # if Cluster.type = 'large_object', no persist
 
     if na_rm is True:
-        nodataval = xr_data.attrs['nodatavals'] 
-        xr_data=xr_data.where(xr_data.sel(band=band) != nodataval)
+        nodataval = xr_data.attrs["nodatavals"]
+        xr_data = xr_data.where(xr_data.sel(band=band) != nodataval)
 
     if persist:
-        print('IMPORTANT: Persisting pulling all data into memory')
+        print("IMPORTANT: Persisting pulling all data into memory")
         xr_data = xr_data.persist()
 
     if filepath != None:
         for func, args in feature_dict.items():
-                
-            feature = [_apply_fun_name(function_name = func,
-                            xr_data=xr_data,
-                            band= band, 
-                            args= arg)
-                                    for arg in args]
-            
-            feature = xr.concat( feature , dim)
 
-            if hasattr(xr_data, 'gw'):
-                feature = feature.gw.match_data(xr_data,  
-                                    band_names=  feature['variable'].values.tolist())
-            
+            feature = [
+                _apply_fun_name(
+                    function_name=func, xr_data=xr_data, band=band, args=arg
+                )
+                for arg in args
+            ]
+
+            feature = xr.concat(feature, dim)
+
+            if hasattr(xr_data, "gw"):
+                feature = feature.gw.match_data(
+                    xr_data, band_names=feature["variable"].values.tolist()
+                )
+
             # out = feature[0]
             # out.gw.imshow()
-            
-            xarray_to_rasterio(feature, path=filepath , postfix=postfix )
+
+            xarray_to_rasterio(feature, path=filepath, postfix=postfix)
 
         return None
 
     else:
-            
-        features = [_apply_fun_name(function_name = func,
-                        xr_data=xr_data ,
-                        band= band, 
-                        args= arg)
-                    for func, args in feature_dict.items() for arg in args]
-        
-        
-        
-        features = xr.concat( features , dim)
-       
-        if hasattr(xr_data, 'gw'):
-            features = features.gw.match_data(xr_data,  
-                                    band_names=  features['variable'].values.tolist())  # place postfix here? or time assigned by dim?
 
-        return features 
+        features = [
+            _apply_fun_name(function_name=func, xr_data=xr_data, band=band, args=arg)
+            for func, args in feature_dict.items()
+            for arg in args
+        ]
 
+        features = xr.concat(features, dim)
+
+        if hasattr(xr_data, "gw"):
+            features = features.gw.match_data(
+                xr_data, band_names=features["variable"].values.tolist()
+            )  # place postfix here? or time assigned by dim?
+
+        return features
 
