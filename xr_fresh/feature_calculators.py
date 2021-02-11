05074dee9ca195d9fb3dd5212541ebe9c68baf80
mmann1123 <mmann1123@gmail.com>
2021-02-11 08:59:34 -0500

    linear_time_trend2 working faster

--- xr_fresh/feature_calculators.py
+++ xr_fresh/feature_calculators.py
@@ -595,7 +595,7 @@ def linear_time_trend(x, param="slope", dim="time", **kwargs):
             t,
             input_core_dims=[[dim], [dim]],
             kwargs={"param": param},
-            vectorize=True,
+            vectorize=False,
             dask="parallelized",
             output_dtypes=[float],
             keep_attrs=True,
@@ -607,46 +607,201 @@ def linear_time_trend(x, param="slope", dim="time", **kwargs):
 # to do: vectorized linreg calc
 # possible example:
 
+
+# def _covariance_gufunc(x, y, x_mean, y_mean, dim="time"):
+#     return ((x - x_mean) * (y - y_mean)).mean(dim=dim)
+
+
+# def _regression_gufunc(x, y, dim="time"):
+#     from scipy.stats import t
+
+#     n = (~np.isnan(ds)).sum(dim="time").values
+
+#     x_std = x.std(dim=dim)
+#     y_std = y.std(dim=dim)
+#     x_mean = x.mean(dim=dim)
+#     y_mean = y.mean(dim=dim)
+#     cov = _covariance_gufunc(x, y, x_mean, y_mean)
+#     cor = cov / (x_std * y_std)
+#     slope = cov / (x_std ** 2)
+#     intercept = y_mean - x_mean * slope
+#     tstats = cor * np.sqrt(n - 2) / np.sqrt(1 - cor ** 2)
+
+#     p_val = t.sf(tstats, n - 2) * 2
+#     # Compute r_square and rmse between time series of x_array and y_array over each (lon,lat) grid box.
+#     # r_square also equals to cor**2 in 1-variable lineare regression analysis, which can be used for checking.
+#     r_square = np.nansum((slope * x + intercept - y_mean) ** 2, axis=0) / np.nansum(
+#         (y - y_mean) ** 2, axis=0
+#     )
+#     # rmse = np.sqrt(np.nansum((y - slope * x - intercept) ** 2, axis=0) / n)
+
+#     return intercept, slope, p_val, r_square
+
+
+def _regression_gufunc2(y_array):
+    from scipy.stats import t
+
+    y_array = np.moveaxis(y_array, -1, 0)
+    x_array = np.empty(y_array.shape)
+    for i in range(y_array.shape[0]):
+        x_array[i, :, :] = (
+            i + 1
+        )  # This would be fine if time series is not too long. Or we can use i+yr (e.g. 2019).
+    x_array[np.isnan(y_array)] = np.nan
+    # Compute the number of non-nan over each (lon,lat) grid box.
+    n = np.sum(~np.isnan(x_array), axis=0)
+    # Compute mean and standard deviation of time series of x_array and y_array over each (lon,lat) grid box.
+    x_mean = np.nanmean(x_array, axis=0)
+    y_mean = np.nanmean(y_array, axis=0)
+    x_std = np.nanstd(x_array, axis=0)
+    y_std = np.nanstd(y_array, axis=0)
+    # Compute co-variance between time series of x_array and y_array over each (lon,lat) grid box.
+    cov = np.nansum((x_array - x_mean) * (y_array - y_mean), axis=0) / n
+    # Compute correlation coefficients between time series of x_array and y_array over each (lon,lat) grid box.
+    cor = cov / (x_std * y_std)
+    # Compute slope between time series of x_array and y_array over each (lon,lat) grid box.
+    slope = cov / (x_std ** 2)
+    # Compute intercept between time series of x_array and y_array over each (lon,lat) grid box.
+    intercept = y_mean - x_mean * slope
+    # Compute tstats, stderr, and p_val between time series of x_array and y_array over each (lon,lat) grid box.
+    tstats = cor * np.sqrt(n - 2) / np.sqrt(1 - cor ** 2)
+
+    p_val = t.sf(tstats, n - 2) * 2
+    # Compute r_square and rmse between time series of x_array and y_array over each (lon,lat) grid box.
+    # r_square also equals to cor**2 in 1-variable lineare regression analysis, which can be used for checking.
+    r_square = np.nansum(
+        (slope * x_array + intercept - y_mean) ** 2, axis=0
+    ) / np.nansum((y_array - y_mean) ** 2, axis=0)
+    # rmse = np.sqrt(np.nansum((y_array - slope * x_array - intercept) ** 2, axis=0) / n)
+    # Do further filteration if needed (e.g. We stipulate at least 3 data records are needed to do regression analysis) and return values
+    n = n * 1.0  # convert n from integer to float to enable later use of np.nan
+    n[n < 3] = np.nan
+    slope[np.isnan(n)] = np.nan
+    intercept[np.isnan(n)] = np.nan
+    p_val[np.isnan(n)] = np.nan
+    r_square[np.isnan(n)] = np.nan
+    # rmse[np.isnan(n)] = np.nan
+
+    return intercept, slope, p_val, r_square
+
+
+def _timereg2(x, param):
+
+    # avoid all missing
+    if allnan(x):
+        if param != "all":
+            return np.NaN
+        else:
+
+            return np.stack((np.NaN, np.NaN, np.NaN, np.NaN), axis=-1)
+
+    else:
+        intercept, slope, pvalue, rvalue = _regression_gufunc2(x)
+
+        if param != "all":
+            KeyError
+            return getattr(linReg, param)
+
+        else:
+            return np.stack((intercept, slope, pvalue, rvalue,), axis=-1,)
+
+
+@set_property("fctype", "ufunc")
+def linear_time_trend2(x, param="all", dim="time", **kwargs):
+
+    if param == "all":
+
+        out = (
+            xr.apply_ufunc(
+                _timereg2,
+                x,
+                input_core_dims=[[dim]],
+                kwargs={"param": param},
+                dask="parallelized",
+                output_dtypes=[float],
+                output_core_dims=[["variable"]],
+                output_sizes={"variable": 4},
+                keep_attrs=True,
+            )
+            .to_dataset(dim="variable")
+            .to_array()
+        )
+    else:
+
+        out = xr.apply_ufunc(
+            _timereg,
+            x,
+            input_core_dims=[[dim]],
+            kwargs={"param": param},
+            dask="parallelized",
+            output_dtypes=[float],
+            keep_attrs=True,
+        )
+
+    return out
+
+
+# @set_property("fctype", "ufunc")
+# def regression_2(x, y, dim="time", **kwargs):
+
+#     return xr.apply_ufunc(
+#         _regression_gufunc,
+#         x,
+#         y,
+#         input_core_dims=[[dim], [dim]],
+#         dask="parallelized",
+#         output_dtypes=[float],
+#         keep_attrs=True,
+#     )
+
+
+# https://stackoverflow.com/questions/52108417/how-to-apply-linear-regression-to-every-pixel-in-a-large-multi-dimensional-array
 # def linregress_3D(y_array):
 #     # y_array is a 3-D array formatted like (time,lon,lat)
 #     # The purpose of this function is to do linear regression using time series of data over each (lon,lat) grid box with consideration of ignoring np.nan
 #     # Construct x_array indicating time indexes of y_array, namely the independent variable.
-#     x_array=np.empty(y_array.shape)
-#     for i in range(y_array.shape[0]): x_array[i,:,:]=i+1 # This would be fine if time series is not too long. Or we can use i+yr (e.g. 2019).
-#     x_array[np.isnan(y_array)]=np.nan
+#     x_array = np.empty(y_array.shape)
+# for i in range(y_array.shape[0]):
+#     x_array[i, :, :] = (
+#         i + 1
+#     )  # This would be fine if time series is not too long. Or we can use i+yr (e.g. 2019).
+#     x_array[np.isnan(y_array)] = np.nan
 #     # Compute the number of non-nan over each (lon,lat) grid box.
-#     n=np.sum(~np.isnan(x_array),axis=0)
+#     n = np.sum(~np.isnan(x_array), axis=0)
 #     # Compute mean and standard deviation of time series of x_array and y_array over each (lon,lat) grid box.
-#     x_mean=np.nanmean(x_array,axis=0)
-#     y_mean=np.nanmean(y_array,axis=0)
-#     x_std=np.nanstd(x_array,axis=0)
-#     y_std=np.nanstd(y_array,axis=0)
+#     x_mean = np.nanmean(x_array, axis=0)
+#     y_mean = np.nanmean(y_array, axis=0)
+#     x_std = np.nanstd(x_array, axis=0)
+#     y_std = np.nanstd(y_array, axis=0)
 #     # Compute co-variance between time series of x_array and y_array over each (lon,lat) grid box.
-#     cov=np.nansum((x_array-x_mean)*(y_array-y_mean),axis=0)/n
+#     cov = np.nansum((x_array - x_mean) * (y_array - y_mean), axis=0) / n
 #     # Compute correlation coefficients between time series of x_array and y_array over each (lon,lat) grid box.
-#     cor=cov/(x_std*y_std)
+#     cor = cov / (x_std * y_std)
 #     # Compute slope between time series of x_array and y_array over each (lon,lat) grid box.
-#     slope=cov/(x_std**2)
+#     slope = cov / (x_std ** 2)
 #     # Compute intercept between time series of x_array and y_array over each (lon,lat) grid box.
-#     intercept=y_mean-x_mean*slope
+#     intercept = y_mean - x_mean * slope
 #     # Compute tstats, stderr, and p_val between time series of x_array and y_array over each (lon,lat) grid box.
-#     tstats=cor*np.sqrt(n-2)/np.sqrt(1-cor**2)
-#     stderr=slope/tstats
+#     tstats = cor * np.sqrt(n - 2) / np.sqrt(1 - cor ** 2)
+#     stderr = slope / tstats
 #     from scipy.stats import t
-#     p_val=t.sf(tstats,n-2)*2
+
+#     p_val = t.sf(tstats, n - 2) * 2
 #     # Compute r_square and rmse between time series of x_array and y_array over each (lon,lat) grid box.
 #     # r_square also equals to cor**2 in 1-variable lineare regression analysis, which can be used for checking.
-#     r_square=np.nansum((slope*x_array+intercept-y_mean)**2,axis=0)/np.nansum((y_array-y_mean)**2,axis=0)
-#     rmse=np.sqrt(np.nansum((y_array-slope*x_array-intercept)**2,axis=0)/n)
+#     r_square = np.nansum(
+#         (slope * x_array + intercept - y_mean) ** 2, axis=0
+#     ) / np.nansum((y_array - y_mean) ** 2, axis=0)
+#     rmse = np.sqrt(np.nansum((y_array - slope * x_array - intercept) ** 2, axis=0) / n)
 #     # Do further filteration if needed (e.g. We stipulate at least 3 data records are needed to do regression analysis) and return values
-#     n=n*1.0 # convert n from integer to float to enable later use of np.nan
-#     n[n<3]=np.nan
-#     slope[np.isnan(n)]=np.nan
-#     intercept[np.isnan(n)]=np.nan
-#     p_val[np.isnan(n)]=np.nan
-#     r_square[np.isnan(n)]=np.nan
-#     rmse[np.isnan(n)]=np.nan
-#     return n,slope,intercept,p_val,r_square,rmse
+#     n = n * 1.0  # convert n from integer to float to enable later use of np.nan
+#     n[n < 3] = np.nan
+#     slope[np.isnan(n)] = np.nan
+#     intercept[np.isnan(n)] = np.nan
+#     p_val[np.isnan(n)] = np.nan
+#     r_square[np.isnan(n)] = np.nan
+#     rmse[np.isnan(n)] = np.nan
+#     return n, slope, intercept, p_val, r_square, rmse
 
 
 # ds = xr.open_dataset('sst_2D.nc', chunks={'X': 30, 'Y': 30})
