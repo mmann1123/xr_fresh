acbf7e464f3319f0d6845efcc778c36f93ab5a7a
mmann1123 <mmann1123@gmail.com>
2024-01-22 10:33:14 -0500

    working simple tests

--- tests/test_series.py
+++ tests/test_series.py
@@ -7,45 +7,59 @@ from datetime import datetime
 import unittest
 import geowombat as gw
 from pathlib import Path
-from xr_fresh.feature_calculator_series import (
-    interpolate_nan_dates,
-    interpolate_nan,
-    doy_of_maximum,
-    abs_energy,
-    abs_energy2,
-    autocorrelation,
-    doy_of_maximum,
-)
+import unittest
+from xr_fresh.feature_calculator_series import abs_energy, maximum
 
-# 202303 is all nan's
-
-# Create a temporary directory
-
-# with tempfile.TemporaryDirectory() as temp_dir:
-#     print(f"Created temporary directory: {temp_dir}")
-# pth = r"../tests/data/"
+# set change directory to location of this file
+pth = os.path.dirname(os.path.abspath(__file__))
+# %%
+os.chdir(pth)
+pth = f"{pth}/data/"
 files = glob(f"{pth}*.tif")
 print(files)
 strp_glob = f"{pth}RadT_tavg_%Y%m.tif"
 dates = sorted(datetime.strptime(string, strp_glob) for string in files)
 date_strings = [date.strftime("%Y-%m-%d") for date in dates]
-date_strings
-# %%
-
 
 # %%
+with gw.series(files) as src:
+    src.apply(abs_energy(), bands=1, num_workers=2, outfile="test2.tif")
+src
 
 
 class TestSeries(unittest.TestCase):
-    def test_series(self):
+    def test_series_abs_energy(self):
         with tempfile.TemporaryDirectory() as tmp:
+            if not os.path.exists(tmp):
+                os.mkdir(tmp)
             out_path = Path(tmp) / "test.tif"
-        with gw.series(files) as src:
-            src.apply(abs_energy(), bands=1, num_workers=2, outfile=out_path)
-        with gw.open(out_path) as dst:
-            self.assertEqual(dst.gw.nbands, 1)
-            self.assertEqual(dst.shape, (1, 1613, 2313))
+            # use rasterio to create a new file tif file
+
+            with gw.series(files) as src:
+                src.apply(abs_energy(), bands=1, num_workers=2, outfile=out_path)
+            with gw.open(out_path) as dst:
+                self.assertEqual(dst.gw.nbands, 1)
+                self.assertEqual(dst.shape, (1, 1613, 2313))
+
+    def test_series_maximum(self):
+        with tempfile.TemporaryDirectory() as tmp:
+            if not os.path.exists(tmp):
+                os.mkdir(tmp)
+            out_path = Path(tmp) / "test.tif"
+            # use rasterio to create a new file tif file
+
+            with gw.series(files) as src:
+                src.apply(maximum(), bands=1, num_workers=2, outfile=out_path)
+            with gw.open(out_path) as dst:
+                self.assertEqual(dst.gw.nbands, 1)
+                self.assertEqual(dst.shape, (1, 1613, 2313))
 
 
 if __name__ == "__main__":
     unittest.main()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+# %%
