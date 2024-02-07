6f5f38416c0ffc431b639d0dbec363e6696e640d
mmann1123 <mmann1123@gmail.com>
2024-02-07 08:53:46 -0500

    more reqs

--- setup.py
+++ setup.py
@@ -35,8 +35,8 @@ else:
 
 REQUIRED = [
     "cython>=0.29.0,<3.0.0",
-    "dask[array,dataframe]>=2022.1.1,<=2022.8.0",
-    "distributed>=2022.1.1,<=2022.8.0",
+    "dask[array,dataframe]>=2023.1.0",
+    "distributed>=2023.1.0",
     "dateparser>=0.7.2",
     "h5netcdf>=0.8.0",
     "h5py>=2.10.0",
