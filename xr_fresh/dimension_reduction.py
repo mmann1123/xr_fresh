0b3101ff0632e3f3c7d3aaf7f33f89ec09d72f30
mmann1123 <mmann1123@gmail.com>
2024-05-16 11:22:11 -0400

    final paralel task woking

--- xr_fresh/dimension_reduction.py
+++ xr_fresh/dimension_reduction.py
@@ -1,27 +1,34 @@
 # %%
-
+# note alternative outlined here: https://github.com/jgrss/geowombat/discussions/318
+import numba
+import ray
 import geowombat as gw
 from glob import glob
-import os
-import itertools
+from geowombat.core.parallel import ParallelTask
+from sklearn.decomposition import KernelPCA
 import numpy as np
-import numba
-from sklearn.decomposition import KernelPCA, PCA
+import numpy as np
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 import cProfile
 
-os.getcwd()
 with gw.open(
-    sorted(glob("./tests/data/R*.tif")), stack_dim="band", band_names=[0, 1, 2, 3]
+    sorted(
+        [
+            "./tests/data/RadT_tavg_202301.tif",
+            "./tests/data/RadT_tavg_202302.tif",
+            "./tests/data/RadT_tavg_202304.tif",
+            "./tests/data/RadT_tavg_202305.tif",
+        ]
+    ),
+    stack_dim="band",
+    band_names=[0, 1, 2, 3],
 ) as src:
     print(src)
     print(src.values.shape)
     src.sel(band=0).gw.imshow()
-# %%
 
-num_features, height, width = src.shape
-data = src.values
+    num_features, height, width = src.shape
+    data = src.values
 
 transposed_data = np.transpose(data, (1, 2, 0))
 features = transposed_data.reshape(-1, num_features)
@@ -29,7 +36,6 @@ features = transposed_data.reshape(-1, num_features)
 # # drop rows with nans
 features = features[~np.isnan(features).any(axis=1)]
 features
-# %%
 
 # Number of random coordinates to select
 num_samples = 10000
@@ -43,6 +49,7 @@ sampled_features = features[
 ]
 
 # Fit Kernel PCA on the sampled features
+print("fitting kpca")
 kpca = KernelPCA(kernel="rbf", gamma=15, n_components=4, n_jobs=-1)
 kpca.fit(sampled_features)
 
@@ -51,12 +58,18 @@ X_fit_ = kpca.X_fit_
 eigenvector = kpca.eigenvectors_[:, 3]  # Take the first component
 eigenvalue = kpca.eigenvalues_[3]
 gamma = kpca.gamma
+# Extract necessary attributes from kpca for transformation
+X_fit_ = kpca.X_fit_
+eigenvector = kpca.eigenvectors_[:, 0]  # Take the first component
+eigenvalue = kpca.eigenvalues_[0]
+gamma = kpca.gamma
+
 # %%
 
 
 @numba.jit(nopython=True, parallel=True)
 def transform_entire_dataset_numba(data, X_fit_, eigenvector, eigenvalue, gamma):
-    num_features, height, width = data.shape
+    height, width = data.shape[1], data.shape[2]
     transformed_data = np.zeros((height, width))
 
     for i in numba.prange(height):
@@ -69,137 +82,48 @@ def transform_entire_dataset_numba(data, X_fit_, eigenvector, eigenvalue, gamma)
     return transformed_data
 
 
-# Apply transformation to the entire datasetaset
-cProfile.run(
-    "transform_entire_dataset_numba(data, X_fit_, eigenvector, eigenvalue, gamma)"
-)
-
-# %%
-# Apply transformation to the entire dataset
-transformed_data = transform_entire_dataset_numba(
-    data, X_fit_, eigenvector, eigenvalue, gamma
-)
-# Reshape to (4000, 3000, 1)
-transformed_data = transformed_data.reshape(height, width, 1)
-
-# Plot the transformed data for visualization
-plt.imshow(transformed_data[:, :, 0], cmap="viridis")
-plt.colorbar()
-plt.title("Transformed Data")
-plt.show()
-
-# %% apply is slower
+@ray.remote
+def process_window(
+    data_block_id,
+    data_slice,
+    window_id,
+    X_fit_,
+    eigenvector,
+    eigenvalue,
+    gamma,
+    num_workers,
+):
+    data_chunk = data_block_id[
+        data_slice
+    ].data.compute()  # Convert Dask array to NumPy array
+    return transform_entire_dataset_numba(
+        data_chunk, X_fit_, eigenvector, eigenvalue, gamma
+    )
 
 
-# class transform_PCA_kernel(gw.TimeModule):
-#     """
-#     Returns the absolute energy of the time series which is the sum over the squared values
+# Open the raster file with GeoWombat
+with gw.open(
+    sorted(
+        [
+            "./tests/data/RadT_tavg_202301.tif",
+            "./tests/data/RadT_tavg_202302.tif",
+            "./tests/data/RadT_tavg_202304.tif",
+            "./tests/data/RadT_tavg_202305.tif",
+        ]
+    ),
+    stack_dim="band",
+    band_names=[0, 1, 2, 3],
+) as src:
+    pt = ParallelTask(src, row_chunks=256, col_chunks=256, scheduler="ray", n_workers=8)
 
-#     Args:
-#         gw (_type_): _description_
-#     """
-
-#     def __init__(self, X_fit_=None, eigenvector=None, eigenvalue=None, gamma=None):
-#         super(transform_PCA_kernel, self).__init__()
-#         self.X_fit_ = X_fit_
-#         self.eigenvector = eigenvector
-#         self.eigenvalue = eigenvalue
-#         self.gamma = gamma
-
-#     def calculate(self, data):
-#         data = data[:, 0, :, :]
-#         num_features, height, width = data.shape
-#         transformed_data = np.zeros((height, width))
-
-#         for i in numba.prange(height):
-#             for j in range(width):
-#                 feature_vector = data[:, i, j]
-#                 k = np.exp(
-#                     -self.gamma * np.sum((feature_vector - self.X_fit_) ** 2, axis=1)
-#                 )
-#                 transformed_feature = np.dot(
-#                     k, self.eigenvector / np.sqrt(self.eigenvalue)
-#                 )
-#                 transformed_data[i, j] = transformed_feature
-
-#         return transformed_data
+    # Map the process_window function to each chunk of the dataset
+    futures = pt.map(process_window, X_fit_, eigenvector, eigenvalue, gamma, 8)
 
 
-# with gw.series(sorted(glob("./tests/data/R*.tif"))) as src:
-#     src.apply(
-#         func=transform_PCA_kernel(
-#             X_fit_=X_fit_,
-#             eigenvector=eigenvector,
-#             eigenvalue=eigenvalue,
-#             gamma=gamma,
-#         ),
-#         outfile="./transformed.tif",
-#         bands=1,
-#     )
-
-# # %%
-# ray.init()
-
-# # %%
-# import ray
-
-
-# # @numba.jit(nopython=True, parallel=True)
-# @ray.remote
-# def transform_entire_dataset_numba(
-#     data, X_fit_, eigenvector, eigenvalue, gamma, height, width
-# ):
-#     data = data.values
-#     # num_features, height, width = data.shape
-#     transformed_data = np.zeros((height, width))
-
-#     for i in numba.prange(height):
-#         for j in range(width):
-#             feature_vector = data[:, i, j]
-#             k = np.exp(-gamma * np.sum((feature_vector - X_fit_) ** 2, axis=1))
-#             transformed_feature = np.dot(k, eigenvector / np.sqrt(eigenvalue))
-#             transformed_data[i, j] = transformed_feature
-
-#     return transformed_data
-
-
-# # Open the raster file with GeoWombat
-# with gw.open(
-#     glob("./tests/data/R*.tif"), stack_dim="band", band_names=[0, 1, 2, 3]
-# ) as src:
-
-#     w = src.gw.windows(256, 256)
-#     print(w)
-#     for i in w:
-#         print(i)
-#         print(type(i))
-#         src.read(1, window=i)
-
-#     # Define the window size
-#     window_size = (256, 256)  # Example window size
-#     height, width = (256, 256)
-#     # Get the dimensions of the raster
-#     height, width = src.gw.nrows, src.gw.ncols
-
-#     # Create a list to store Ray tasks
-#     ray_tasks = []
-
-#     for w in src.gw.windows(256, 256):
-
-#         # Submit the Ray task
-#         ray_tasks.append(
-#             transform_entire_dataset_numba.remote(
-#                 w, X_fit_, eigenvector, eigenvalue, gamma, height, width
-#             )
-#         )
-#     # Get the results from Ray tasks
-#     results = ray.get(ray_tasks)
-
-#     # Process the results (e.g., print the mean values)
-#     for result in results:
-#         print(result)
-
-# # %%
-# ray.shutdown()
-
-# # %%
+# Combine the results
+transformed_data = np.zeros((height, width))
+for window_id, future in enumerate(ray.get(futures)):
+    window = pt.windows[window_id]
+    row_start, col_start = window.row_off, window.col_off
+    row_end, col_end = row_start + window.height, col_start + window.width
+    transformed_data[row_start:row_end, col_start:col_end] = future
