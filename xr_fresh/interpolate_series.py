b2780841ee7d0fc3c0bd5b6904b0e1946720eb2f
Jithendra <jithedrakatta999@gmail.com>
2024-06-10 18:26:57 -0400

    updated tests and interpolate_series

--- xr_fresh/interpolate_series.py
+++ xr_fresh/interpolate_series.py
@@ -722,3 +722,126 @@ class interpolate_nan(gw.TimeModule):
 #             sign = -1
 #         xs = np.array([a, b])
 #         return sign * np.diff(self.antiderivative(xs))
+
+#===========================================================================================================================
+
+
+# import jax.numpy as jnp
+# import numpy as np
+# from scipy.interpolate import interp1d, CubicSpline, UnivariateSpline
+# from datetime import datetime
+# import geowombat as gw
+
+# class interpolate_nan(gw.TimeModule):
+#     def __init__(self, missing_value=None, interp_type="linear", count=1, dates=None):
+#         super(interpolate_nan, self).__init__()
+#         if dates is None:
+#             print("NOTE: Dates are unknown, assuming regular time interval")
+#             self.dates = dates
+#         elif not isinstance(dates, list) or not all(isinstance(d, datetime) for d in dates):
+#             raise TypeError("dates must be a list of datetime objects")
+#         else:
+#             print("NOTE: Dates will be used to index the time series for interpolation")
+#             self.dates = dates
+#             self.date_indices = np.array([(date - self.dates[0]).days for date in self.dates])
+        
+#         self.missing_value = missing_value
+#         self.interp_type = interp_type
+#         self.count = count
+
+#     @staticmethod
+#     def _interpolate_nans_interp1d(array, kind):
+#         nan_check = np.isnan(array)
+#         if all(nan_check) or all(~nan_check):
+#             return array
+#         else:
+#             valid_indices = np.where(~nan_check)[0]
+#             valid_values = array[valid_indices]
+#             inter_fun = interp1d(valid_indices, valid_values, kind=kind, bounds_error=False, fill_value="extrapolate")
+#             return inter_fun(np.arange(len(array)))
+
+#     def _interpolate_nans_interp1d_with_dates(self, array):
+#         nan_check = np.isnan(array)
+#         if all(nan_check) or all(~nan_check):
+#             return array
+#         else:
+#             valid_indices = np.where(~nan_check)[0]
+#             valid_dates = self.date_indices[valid_indices]
+#             valid_values = array[valid_indices]
+#             inter_fun = interp1d(valid_dates, valid_values, kind=self.interp_type, fill_value="extrapolate")
+#             return inter_fun(self.date_indices)
+
+#     @staticmethod
+#     def _interpolate_nans_CubicSpline(array):
+#         nan_check = np.isnan(array)
+#         if all(nan_check) or all(~nan_check):
+#             return array
+#         else:
+#             valid_indices = np.where(~nan_check)[0]
+#             valid_values = array[valid_indices]
+#             inter_fun = CubicSpline(valid_indices, valid_values, bc_type="not-a-knot")
+#             return inter_fun(np.arange(len(array)))
+
+#     def _interpolate_nans_CubicSpline_with_dates(self, array):
+#         nan_check = np.isnan(array)
+#         if all(nan_check) or all(~nan_check):
+#             return array
+#         else:
+#             valid_indices = np.where(~nan_check)[0]
+#             valid_dates = self.date_indices[valid_indices]
+#             valid_values = array[valid_indices]
+#             inter_fun = CubicSpline(valid_dates, valid_values, bc_type="not-a-knot")
+#             return inter_fun(self.date_indices)
+
+#     @staticmethod
+#     def _interpolate_nans_UnivariateSpline(array, s=1):
+#         nan_check = np.isnan(array)
+#         if all(nan_check) or all(~nan_check):
+#             return array
+#         else:
+#             valid_indices = np.where(~nan_check)[0]
+#             valid_values = array[valid_indices]
+#             inter_fun = UnivariateSpline(valid_indices, valid_values, s=s)
+#             return inter_fun(np.arange(len(array)))
+
+#     def _interpolate_nans_UnivariateSpline_with_dates(self, array, s=1):
+#         nan_check = np.isnan(array)
+#         if all(nan_check) or all(~nan_check):
+#             return array
+#         else:
+#             valid_indices = np.where(~nan_check)[0]
+#             valid_date_indices = self.date_indices[valid_indices]
+#             valid_values = array[valid_indices]
+#             inter_fun = UnivariateSpline(valid_date_indices, valid_values, s=s)
+#             return inter_fun(self.date_indices)
+
+#     def calculate(self, array):
+#         if self.missing_value is not None:
+#             if not np.isnan(self.missing_value):
+#                 array = np.where(array == self.missing_value, np.NaN, array)
+        
+#         if self.interp_type == "linear":
+#             if self.dates is None:
+#                 array = np.apply_along_axis(self._interpolate_nans_interp1d, axis=0, arr=array, kind="linear")
+#             else:
+#                 array = np.apply_along_axis(self._interpolate_nans_linear_with_dates, axis=0, arr=array, self=self)
+        
+#         elif self.interp_type in ["nearest", "zero", "slinear", "quadratic", "cubic", "previous", "next"]:
+#             if self.dates is None:
+#                 array = np.apply_along_axis(self._interpolate_nans_interp1d, axis=0, arr=array, kind=self.interp_type)
+#             else:
+#                 array = np.apply_along_axis(self._interpolate_nans_interp1d_with_dates, axis=0, arr=array, self=self)
+
+#         elif self.interp_type in ["cubicspline", "spline"]:
+#             if self.dates is None:
+#                 array = np.apply_along_axis(self._interpolate_nans_CubicSpline, axis=0, arr=array)
+#             else:
+#                 array = np.apply_along_axis(self._interpolate_nans_CubicSpline_with_dates, axis=0, arr=array, self=self)
+
+#         elif self.interp_type == "UnivariateSpline":
+#             if self.dates is None:
+#                 array = np.apply_along_axis(self._interpolate_nans_UnivariateSpline, axis=0, arr=array, s=1)
+#             else:
+#                 array = np.apply_along_axis(self._interpolate_nans_UnivariateSpline_with_dates, axis=0, arr=array, self=self, s=1)
+
+#         return array.squeeze()
