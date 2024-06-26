c66b8946c28fc1e486689d3532e1d0dae2ce01ba
mmann1123 <mmann1123@gmail.com>
2024-06-26 14:49:21 -0400

    clean

--- tests/test_extractors_series.py
+++ tests/test_extractors_series.py
@@ -1,20 +1,20 @@
 import unittest
-from unittest.mock import patch, MagicMock
-from xr_fresh.extractors_series import extract_features_series
+
+# from unittest.mock import patch, MagicMock
+# from xr_fresh.extractors_series import extract_features_series
 
 
-
-#@patch: This decorator is used to replace an object with a mock object for the duration of a test function. 
-#        It allows you to temporarily replace the specified object (usually a function or class) with a mock version. 
-#        When the test function finishes, the original object is restored. This is useful for isolating the behavior of a specific function 
-#        or method without affecting other parts of the code. In the provided example, @patch is used to mock the xr module 
+# @patch: This decorator is used to replace an object with a mock object for the duration of a test function.
+#        It allows you to temporarily replace the specified object (usually a function or class) with a mock version.
+#        When the test function finishes, the original object is restored. This is useful for isolating the behavior of a specific function
+#        or method without affecting other parts of the code. In the provided example, @patch is used to mock the xr module
 #        and the _apply_fun_name function during the execution of the test cases.
 
-#MagicMock: This is a subclass of Mock that provides default implementations of all magic methods. It allows you to 
-#           create mock objects with minimal setup, and it automatically generates stubs for all attribute accesses and method calls. 
-#           MagicMock objects are highly flexible and can be used to simulate the behavior of any object or function. 
-#           In the provided example, MagicMock is used to create mock objects for the xarray.DataArray class and its methods, 
-#           which are then used as the return values of the mocked xr module. This allows the test cases to simulate the behavior of 
+# MagicMock: This is a subclass of Mock that provides default implementations of all magic methods. It allows you to
+#           create mock objects with minimal setup, and it automatically generates stubs for all attribute accesses and method calls.
+#           MagicMock objects are highly flexible and can be used to simulate the behavior of any object or function.
+#           In the provided example, MagicMock is used to create mock objects for the xarray.DataArray class and its methods,
+#           which are then used as the return values of the mocked xr module. This allows the test cases to simulate the behavior of
 #           xarray.DataArray objects without actually creating them.
 
 
@@ -72,5 +72,5 @@ from xr_fresh.extractors_series import extract_features_series
 #         self.assertEqual(mock_gw.series.call_count, 1)
 #         self.assertEqual(mock_series.apply.call_count, 1)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
