188054d67c47511eff50c7aa685e46b48f988fc0
mmann1123 <mmann1123@gmail.com>
2024-07-24 15:29:01 -0400

    add docs back to baseurl

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,7 +80,7 @@ html_theme = "sphinx_rtd_theme"  #'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_baseurl = "https://mmann1123.github.io/xr_fresh"
+html_baseurl = "https://mmann1123.github.io/xr_fresh/docs/"
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
