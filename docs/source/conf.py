eccddc4bc4f478aa37ab68053567f0daafcfdeb2
mmann1123 <mmann1123@gmail.com>
2024-07-24 16:28:47 -0400

    mock install for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,9 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# pretend to install the following packages
+autodoc_mock_imports = ["xr_fresh", "numpy", "geowombat", "gdal"]
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
