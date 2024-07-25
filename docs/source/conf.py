1746cac8e443151d686244ff8f8f435da14c7851
mmann1123 <mmann1123@gmail.com>
2024-07-25 00:05:11 -0400

    remove /docs/ from base url WRONG

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_baseurl = "https://mmann1123.github.io/xr_fresh/docs/"
+html_baseurl = "https://mmann1123.github.io/xr_fresh/"
 
 html_theme_options = {
     "canonical_url": "",
