e234653c3f8273621fc0af82debe62a34e2bcef5
mmann1123 <mmann1123@gmail.com>
2024-07-25 00:18:18 -0400

    "../_static"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,7 +68,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["https://mmann1123.github.io/xr_fresh/_static"]
+html_static_path = ["../_static"]
 html_baseurl = "https://mmann1123.github.io/xr_fresh/"
 
 html_theme_options = {
